38110e26abb3ee834e025caa6b362ff4b299b2bd
Steven Gao <steven@stevengao.dev>
2023-10-27 22:06:13 -0700

    Use BrowserRouter for easier shareable links

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
-// import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
